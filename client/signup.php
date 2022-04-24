5482a07d26d4ac2c0b7df02bc07f5a93a9776603
Rouen Quirino <rouen2101@gmail.com>
2022-04-24 11:15:56 +0800

    add needed input fields in signup page

--- client/signup.php
+++ client/signup.php
@@ -19,29 +19,64 @@
                     <h1 class="card-title mb-4">Sign Up</h1>
 
                     <form action="" method="post">
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="inputGroup-sizing-sm">Username</span>
+
+                        <div class="mb-3">
+                            <label for="fname-input" class="form-label">First Name</label>
+                            <input id="fname-input" type="text" class="form-control" name="fname" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="lname-input" class="form-label">Last Name</label>
+                            <input id="lname-input" type="text" class="form-control" name="lname" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="gender-input" class="form-label">Gender</label>
+                                <select name="gender" id="gender-input"  class="form-select" required>
+                                    <option value="Male">Male</option>
+                                    <option value="Female">Female</option>
+                                </select>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="bday-input" class="form-label">Birhtday</label>
+                            <input id="bday-input" type="date" class="form-control" name="bday" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="bday-input" class="form-label">Address</label>
+                            <input id="bday-input" type="text" class="form-control" name="address" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="bday-input" class="form-label">Email</label>
+                            <input id="bday-input" type="email" class="form-control" name="email" required>
+                        </div>
+
+
+
+
+                        <div class="mb-3 mt-5">
+                            <label class="form-label">Username</label>
                             <input type="text" class="form-control" name="username" required>
                         </div>
 
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="inputGroup-sizing-sm">Password</span>
+                        <div class="mb-3">
+                            <label class="form-label">Password</label>
                             <input type="password" class="form-control" name="password" required>
                         </div>
 
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="inputGroup-sizing-sm">Confirm Password</span>
+                        <div class="mb-3">
+                            <label class="form-label">Confirm Password</label>
                             <input type="password" class="form-control" name="conPassword" required>
                         </div>
 
                         <?php if(isset($errorMsg)){ echo $errorMsg;}?>
-
-                        <a class="btn btn-outline-dark me-3" href="index.php">
-                        <i class="bi bi-arrow-left-circle"></i> back</a>
                         
                         <button type="submit" class="btn btn-primary" name="signup">Submit</button>
-
                     </form>
+
+                    <p class="mt-4 text-black-50">Have already an account? <a href="login.php">login here</a></p>
                 </div>
             </div>
         </div>
