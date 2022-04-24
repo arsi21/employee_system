979b14a49bb6d1242337f60836c0d59836192656
Rouen Quirino <rouen2101@gmail.com>
2022-04-24 19:45:54 +0800

    fix delete function

--- server/delete.php
+++ server/delete.php
@@ -9,9 +9,12 @@ if(isset($_POST['delete'])){
     $id = $_POST['id'];
 }
 
-//for displaying all the names
-$sql = "DELETE FROM employee_info WHERE id = '$id'";
-$con->query($sql) or die ($con->error);
+//for deleting details
+$deleteEmp = "DELETE FROM employee_info WHERE id = '$id'";
+$con->query($deleteEmp) or die ($con->error);
+
+$deleteUser = "DELETE FROM user WHERE employee_id = '$id'";
+$con->query($deleteUser) or die ($con->error);
 
 header("Location: ../client/index.php");
 
