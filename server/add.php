e3006e644e8268e0a4e226e666b031350c3af17f
Rouen Quirino <rouen2101@gmail.com>
2022-04-26 10:40:14 +0800

    fix the bug when adding empty employee

--- server/add.php
+++ server/add.php
@@ -8,23 +8,30 @@ $con = connection();
 //check if the submit button is clicked
 if(isset($_POST['submit'])){
 
-    //get data from the form
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $gender = $_POST['gender'];
-    $bday = $_POST['bday'];
-    $address = $_POST['address'];
-    $email = $_POST['email'];
+    //check if all input fields have value
+    if(!empty($_POST['fname']) && !empty($_POST['lname']) && !empty($_POST['gender']) && !empty($_POST['bday']) && !empty($_POST['address']) && !empty($_POST['email'])){
 
-    //query for inserting data into database
-    $sql = "INSERT INTO `employee_info`(`first_name`, `last_name`, 
-    `gender`, `birthday`, `address`, `email`) VALUES ('$fname', '$lname', '$gender', '$bday', '$address', '$email')";
+        //get data from the form
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $gender = $_POST['gender'];
+        $bday = $_POST['bday'];
+        $address = $_POST['address'];
+        $email = $_POST['email'];
 
-    //insert data into database
-    $con->query($sql) or die ($con->error);
+        //query for inserting data into database
+        $sql = "INSERT INTO `employee_info`(`first_name`, `last_name`, 
+        `gender`, `birthday`, `address`, `email`) VALUES ('$fname', '$lname', '$gender', '$bday', '$address', '$email')";
 
-    //redirect to index.php
-    header("Location: ../client/index.php");
+        //insert data into database
+        $con->query($sql) or die ($con->error);
+
+        //redirect to index.php
+        header("Location: ../client/index.php");
+    }else{
+        //redirect to index.php
+        header("Location: ../client/index.php");
+    }
 }
 
 ?>
