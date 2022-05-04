3a3f8e678be6f5ec22723c902677260b48a59690
Rouen Quirino <rouen2101@gmail.com>
2022-05-04 09:49:07 +0800

    add regex and error handling in editing employee details

--- details.php
+++ details.php
@@ -5,13 +5,14 @@
 
 <!-- add client partials -->
 <?php include_once("partials/header.php")?>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 
     <div class="container">
         <div class="d-flex mt-2 mb-3">
             <a class="btn btn-outline-dark" href="index.php">
             <i class="bi bi-arrow-left-circle"></i> back</a>
 
-            <button class="btn btn-secondary mx-2"  data-bs-toggle="modal" data-bs-target="#editStudentModal" >Edit</button>
+            <button class="btn btn-secondary mx-2"  data-bs-toggle="modal" data-bs-target="#editEmployeeModal" >Edit</button>
             
             <?php 
                 //start session
@@ -54,7 +55,7 @@
 
      <!-- Modal -->
      <form action="server/edit-details.php" method="get">
-        <div class="modal fade" id="editStudentModal" tabindex="-1">
+        <div class="modal fade" id="editEmployeeModal" tabindex="-1">
         <div class="modal-dialog">
             <div class="modal-content">
             <div class="modal-header">
@@ -65,38 +66,179 @@
 
                 <input type="hidden" name="id"  value="<?php echo $row['id'];?>" required>
 
-                <div class="mb-3">
-                    <label for="fname-input" class="form-label">First Name</label>
-                    <input id="fname-input" type="text" class="form-control" name="fname"  value="<?php echo $row['first_name'];?>" required>
-                </div>
 
-                <div class="mb-3">
-                    <label for="lname-input" class="form-label">Last Name</label>
-                    <input id="lname-input" type="text" class="form-control" name="lname" value="<?php echo $row['last_name'];?>" required>
-                </div>
 
-                <div class="mb-3">
-                    <label for="gender-input" class="form-label">Gender</label>
-                        <select name="gender" id="gender-input" class="form-select" required>
-                            <option value="Male" <?php echo ($row['gender'] == "Male")? 'selected' : '';?>>Male</option>
-                            <option value="Female" <?php echo ($row['gender'] == "Female")? 'selected' : '';?>>Female</option>
-                        </select>
-                </div>
+                <?php 
+                    //for showing error
+                    if(isset($_GET['errorMsg'])){
+                        $errorMsg = $_GET['errorMsg'];
 
-                <div class="mb-3">
-                    <label for="bday-input" class="form-label">Birhtday</label>
-                    <input id="bday-input" type="date" class="form-control" name="bday" value="<?php echo $row['birthday'];?>" required>
-                </div>
+                        if($errorMsg == "fname"){
+                            echo '
+                                <div class="alert alert-warning" role="alert">
+                                    First name contains an invalid character!
+                                </div>
+                            ';
+                        }elseif($errorMsg == "lname"){
+                            echo '
+                                <div class="alert alert-warning" role="alert">
+                                    Last name contains an invalid character!
+                                </div>
+                            ';
+                        }elseif($errorMsg == "gender"){
+                            echo '
+                                <div class="alert alert-warning" role="alert">
+                                    Invalid gender!
+                                </div>
+                            ';
+                        }elseif($errorMsg == "bday"){
+                            echo '
+                                <div class="alert alert-warning" role="alert">
+                                    Invalid birthday!
+                                </div>
+                            ';
+                        }elseif($errorMsg == "address"){
+                            echo '
+                                <div class="alert alert-warning" role="alert">
+                                    Address contains an invalid character!
+                                </div>
+                            ';
+                        }elseif($errorMsg == "email"){
+                            echo '
+                                <div class="alert alert-warning" role="alert">
+                                    Email contains an invalid character!
+                                </div>
+                            ';
+                        }elseif($errorMsg == "emptyField"){
+                            echo '
+                                <div class="alert alert-warning" role="alert">
+                                    Fill up all fields!
+                                </div>
+                            ';
+                        }
+                    }
+                ?>
 
-                <div class="mb-3">
-                    <label for="grade-input" class="form-label">Address</label>
-                    <input id="grade-input" type="text" class="form-control" name="address" value="<?php echo $row['address'];?>" required>
-                </div>
 
-                <div class="mb-3">
-                    <label for="grade-input" class="form-label">Email</label>
-                    <input id="grade-input" type="email" class="form-control" name="email" value="<?php echo $row['email'];?>" required>
-                </div>
+
+
+
+
+                <?php
+                    //for showing the inputed value
+                    if(isset($_GET['fname']) || isset($_GET['lname']) || isset($_GET['gender']) || isset($_GET['bday']) || isset($_GET['address']) || isset($_GET['email'])){
+                        //get data from the add-user
+                        if(isset($_GET['fname'])){
+                            $fname = $_GET['fname'];
+                        }else{
+                            $fname = "";
+                        }
+
+                        if(isset($_GET['lname'])){
+                            $lname = $_GET['lname'];
+                        }else{
+                            $lname = "";
+                        }
+
+                        if(isset($_GET['gender'])){
+                            $gender = $_GET['gender'];
+                        }else{
+                            $gender = "";
+                        }
+
+                        if(isset($_GET['bday'])){
+                            $bday = $_GET['bday'];
+                        }else{
+                            $bday = "";
+                        }
+
+                        if(isset($_GET['address'])){
+                            $address = $_GET['address'];
+                        }else{
+                            $address = "";
+                        }
+
+                        if(isset($_GET['email'])){
+                            $email = $_GET['email'];
+                        }else{
+                            $email = "";
+                        }
+                ?>
+                        <div class="mb-3">
+                            <label for="fname-input" class="form-label">First Name</label>
+                            <input id="fname-input" type="text" class="form-control" name="fname" value="<?php echo $fname;?>" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="lname-input" class="form-label">Last Name</label>
+                            <input id="lname-input" type="text" class="form-control" name="lname" value="<?php echo $lname;?>" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="gender-input" class="form-label">Gender</label>
+                                <select name="gender" id="gender-input"  class="form-select" required>
+                                    <option value="Male" <?php echo ($gender == "Male")? 'selected' : '';?>>Male</option>
+                                    <option value="Female" <?php echo ($gender == "Female")? 'selected' : '';?>>Female</option>
+                                </select>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="bday-input" class="form-label">Birhtday</label>
+                            <input id="bday-input" type="date" class="form-control" name="bday" value="<?php echo $bday;?>" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="bday-input" class="form-label">Address</label>
+                            <input id="bday-input" type="text" class="form-control" name="address" value="<?php echo $address;?>" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="bday-input" class="form-label">Email</label>
+                            <input id="bday-input" type="email" class="form-control" name="email" value="<?php echo $email;?>" required>
+                        </div>
+                <?php
+                    }else{
+                ?>
+                        <!-- for showing input fields -->
+                        <div class="mb-3">
+                            <label for="fname-input" class="form-label">First Name</label>
+                            <input id="fname-input" type="text" class="form-control" name="fname"  value="<?php echo $row['first_name'];?>" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="lname-input" class="form-label">Last Name</label>
+                            <input id="lname-input" type="text" class="form-control" name="lname" value="<?php echo $row['last_name'];?>" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="gender-input" class="form-label">Gender</label>
+                                <select name="gender" id="gender-input" class="form-select" required>
+                                    <option value="Male" <?php echo ($row['gender'] == "Male")? 'selected' : '';?>>Male</option>
+                                    <option value="Female" <?php echo ($row['gender'] == "Female")? 'selected' : '';?>>Female</option>
+                                </select>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="bday-input" class="form-label">Birhtday</label>
+                            <input id="bday-input" type="date" class="form-control" name="bday" value="<?php echo $row['birthday'];?>" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="grade-input" class="form-label">Address</label>
+                            <input id="grade-input" type="text" class="form-control" name="address" value="<?php echo $row['address'];?>" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="grade-input" class="form-label">Email</label>
+                            <input id="grade-input" type="email" class="form-control" name="email" value="<?php echo $row['email'];?>" required>
+                        </div>
+                <?php
+                    }
+                ?>
+
+
+
+
             
             </div>
             <div class="modal-footer">
@@ -111,6 +253,14 @@
 
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script>
+        //open modal if have an error
+        if(<?php echo(isset($_GET['errorMsg']));?>){
+            $(document).ready(function(){
+                $("#editEmployeeModal").modal('show');
+            });
+        }
+    </script>
 
 <!-- add client partials -->
 <?php include_once("partials/footer.php")?>
--- server/edit-details.php
+++ server/edit-details.php
@@ -18,21 +18,101 @@ if(isset($_GET['edit'])){
     $address = $_GET['address'];
     $email = $_GET['email'];
 
-    //query for updating data into database
-    $sql = "UPDATE `employee_info` SET 
-    first_name = '$fname', 
-    last_name = '$lname', 
-    gender = '$gender', 
-    birthday = '$bday',
-    address = '$address',
-    email = '$email'
-    WHERE id = '$id'";
 
-    //insert data into database
-    $con->query($sql) or die ($con->error);
+    
+    //check if all input fields have value
+    if(!empty($_GET['fname']) && !empty($_GET['lname']) && !empty($_GET['gender']) && !empty($_GET['bday']) && !empty($_GET['address']) && !empty($_GET['email'])){
+        //regex pattern
+        $namePattern = "/^\s+|\s+$|\s{2,}|[0-9]+|^\-+|\-$|\-{2,}|[^a-zA-Z\s\-]/";
+        $bdayPattern = "/^[\d\-]+$/";
+        $addressPattern = "/^[a-zA-Z\d\s\-\_\.\(\)\#]+$/";
+        $emailPattern = "/^[a-zA-Z\d\-\_\.]+@[a-zA-Z\d]+\.[a-zA-Z\.]+$/";
 
-    //redirect to details.php
-    header("Location: ../details.php?id=".$id);
+        //check if all input are valid
+        if(preg_match($namePattern, $fname) == 0){
+            $isFnameValid = true;
+        }else{
+            $isFnameValid = false;
+
+            //redirect to details.php
+            header("Location: ../details.php?errorMsg=fname&id=$id&lname=$lname&gender=$gender&bday=$bday&address=$address&email=$email");
+            exit();
+        }
+
+        if(preg_match($namePattern, $lname) == 0){
+            $isLnameValid = true;
+        }else{
+            $isLnameValid = false;
+
+            //redirect to details.php
+            header("Location: ../details.php?errorMsg=lname&id=$id&fname=$fname&gender=$gender&bday=$bday&address=$address&email=$email");
+            exit();
+        }
+
+        if($gender == "Male" || $gender == "Female"){
+            $isGenderValid = true;
+        }else{
+            $isGenderValid = false;
+
+            //redirect to details.php
+            header("Location: ../details.php?errorMsg=gender&id=$id&fname=$fname&lname=$lname&bday=$bday&address=$address&email=$email");
+            exit();
+        }
+
+        if(preg_match($bdayPattern, $bday)){
+            $isBdayValid = true;
+        }else{
+            $isBdayValid = false;
+
+            //redirect to details.php
+            header("Location: ../details.php?errorMsg=bday&id=$id&fname=$fname&lname=$lname&gender=$gender&address=$address&email=$email");
+            exit();
+        }
+
+        if(preg_match($addressPattern, $address)){
+            $isAddressValid = true;
+        }else{
+            $isAddressValid = false;
+
+            //redirect to details.php
+            header("Location: ../details.php?errorMsg=address&id=$id&fname=$fname&lname=$lname&gender=$gender&bday=$bday&email=$email");
+            exit();
+        }
+
+        if(preg_match($emailPattern, $email)){
+            $isEmailValid = true;
+        }else{
+            $isEmailValid = false;
+
+            //redirect to details.php
+            header("Location: ../details.php?errorMsg=email&id=$id&fname=$fname&lname=$lname&gender=$gender&bday=$bday&address=$address");
+            exit();
+        }
+
+        
+
+        if($isFnameValid && $isLnameValid && $isGenderValid && $isBdayValid && $isAddressValid && $isEmailValid){
+        //query for updating data into database
+        $sql = "UPDATE `employee_info` SET 
+        first_name = '$fname', 
+        last_name = '$lname', 
+        gender = '$gender', 
+        birthday = '$bday',
+        address = '$address',
+        email = '$email'
+        WHERE id = '$id'";
+
+        //insert data into database
+        $con->query($sql) or die ($con->error);
+
+        //redirect to details.php
+        header("Location: ../details.php?id=".$id);
+
+        }
+    }else{
+        //redirect to details.php
+        header("Location: ../details.php?errorMsg=emptyField&id=$id&fname=$fname&lname=$lname&gender=$gender&bday=$bday&address=$address&email=$email");
+    }
 }
 
 ?>
